7b552d51f2fcce9123131fa460b4dd993e781982
Abhishek <kumarabhishek0388@gmail.com>
2020-04-16 12:13:42 +0530

    into lab

---
+++ JavaScriptIntro.js
@@ -0,0 +1,39 @@
+console.log("Hello From first JS program !!!");
+
+for(var x=0; x<=10;x++) {
+    console.log('Hello I am loop')
+}
+
+
+var x = [1, 3, 5, 8];
+console.log(x)
+
+var y  = function () {
+    console.log('Hello I am a function')
+    return 'hellos i am functions return value'
+}();
+
+console.log(y)
+
+const firsName = "Abhishek";
+const lastName = 'Kumar'
+
+
+const val = 42;
+
+const myArr = [
+    'String',
+    31,
+    function() {
+        console.log('hello from function')
+    }
+]   
+
+myArr[2]();
+
+
+for(let z=0; z<=4;z++) {
+    console.log('Hello I am loop')
+}
+
+//console.log('lets check the value of let : ' + z)
