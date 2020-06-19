6b0bf674eb1239d56f72e35a9f3894a4f6271375
Abhishek <kumarabhishek0388@gmail.com>
2020-06-19 13:38:06 +0530

    fp2

--- functionalProg.js
+++ functionalProg.js
@@ -1,4 +1,4 @@
-// Functions are first class citizens
+// Functions are first class citizens //
 
 function sayHello() {
     console.log("Hello from  my function!!!");
@@ -6,3 +6,22 @@ function sayHello() {
 
 const func =  sayHello;
 func();
+
+//1) functions as an argument to another function
+
+function greet(fnMessage) {
+    console.log(fnMessage());
+}
+
+greet(sayHello);
+
+// returning a function from a function
+
+function dummy() {
+    return function() {
+        return "hello world";
+    }
+}
+
+const x = dummy();
+console.log(x());
